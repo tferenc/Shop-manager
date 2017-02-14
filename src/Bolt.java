ee6d48cc31d1f4abf60331ed3982c45c24402526
tferenc <tamasferenc90@gmail.com>
2017-02-14 13:17:28 +0100

    Bolt and Tej classes done till 2.4

--- src/Bolt.java
+++ src/Bolt.java
@@ -69,5 +69,51 @@ public class Bolt
             tejpult.put(m, 1);
         }
     }
+    class BoltBejegyzes
+    {
+        private Tej t;
+        private int mennyiseg;
+        private int ar;
 
+        public BoltBejegyzes(Tej t, int mennyiseg, int ar)
+        {
+            this.t = t;
+            this.mennyiseg = mennyiseg;
+            this.ar = ar;
+        }
+
+        public Tej getT() {
+            return t;
+        }
+
+        public void setT(Tej t) {
+            this.t = t;
+        }
+
+        public int getMennyiseg() {
+            return mennyiseg;
+        }
+
+        public void setMennyiseg(int mennyiseg) {
+            this.mennyiseg = mennyiseg;
+        }
+
+        public int getAr() {
+            return ar;
+        }
+
+        public void setAr(int ar) {
+            this.ar = ar;
+        }
+
+        public void adMennyiseg(int mennyiseg)
+        {
+            this.mennyiseg += mennyiseg;
+        }
+
+        public void levonMennyiseg(int mennyiseg)
+        {
+            this.mennyiseg -= mennyiseg;
+        }
+    }
 }
