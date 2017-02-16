836177b8a4d36df173d0ef82e4281b8489daabbc
tferenc <tamasferenc90@gmail.com>
2017-02-16 10:21:47 +0100

    NemLetezoAruKivetel and TulSokLevonasKivetel classes created. 4.0.

---
+++ src/NemLetezoAruKivetel.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tamasferenc on 2017.02.16..
+ */
+public class NemLetezoAruKivetel extends Exception
+{
+    public NemLetezoAruKivetel(String messasge)
+    {
+
+    }
+}
---
+++ src/TulSokLevonasKivetel.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tamasferenc on 2017.02.16..
+ */
+public class TulSokLevonasKivetel extends Exception
+{
+    public TulSokLevonasKivetel(String message)
+    {
+
+    }
+}
